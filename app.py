daa50f6d048422af2927c63be2b60c1decabf360
hussain <hussainaradi.ha@gmail.com>
2024-07-04 17:33:07 +0300

    setUp the database

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from resources.users import Users, UserDetails
 
 app = Flask(__name__)
 
-# Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hussain:admin@localhost:5432/'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hussain:admin@localhost:5432/magicinspector'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
@@ -16,9 +15,8 @@ with app.app_context():
 
 api = Api(app)
 
-# Define the API routes
 api.add_resource(Users, '/users')
-api.add_resource(UserDetails, '/users/<int:id>')
+api.add_resource(UserDetails, '/users/<uuid:id>') 
 
 if __name__ == '__main__':
     app.run(debug=True)
