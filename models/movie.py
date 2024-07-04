3195b7bb428c171fd6a2fae3c5d6af17a495324b
Ghazialsardeeh <ghazialbakar@gmail.com>
2024-07-04 02:26:45 -0700

    movie model

--- models/movie.py
+++ models/movie.py
@@ -9,4 +9,18 @@ class Movie(db.Model):
     genre = db.Column(db.String, nullable=False)
     theatre_ids = db.Column(db.PickleType, nullable=False)
 
+    def __init__(self, id, name, img, genre, theatre_ids):
+        self.id = id
+        self.name = name
+        self.img = img
+        self.genre = genre
+        self.theatre_ids = theatre_ids
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'img': self.img,
+            'genre': self.genre,
+            'theatre_ids': self.theatre_ids
+        }
