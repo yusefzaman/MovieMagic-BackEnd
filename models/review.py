fd86f7a07e5a0ca6eb8aaf8e0234135ccc0b5c74
hussain <hussainaradi.ha@gmail.com>
2024-07-06 01:26:36 +0300

    create the Review class

--- models/review.py
+++ models/review.py
@@ -1,4 +1,17 @@
 from db import db
 from datetime import datetime
-from models.user import User  
-from models.movie import Movie  
+from models.user import User
+from models.movie import Movie
+
+
+class Review(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    user = db.relationship("User", back_populates="reviews")
+
+    movie_id = db.Column(db.Integer, db.ForeignKey("movies.id"), nullable=False)
+    movie = db.relationship("Movie", back_populates="reviews")
