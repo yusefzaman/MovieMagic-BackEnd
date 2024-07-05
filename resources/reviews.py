fd7f3eec5db0548db0f550dba984ec5087e4ed64
hussain <hussainaradi.ha@gmail.com>
2024-07-06 01:56:25 +0300

    some implemantion in the reviwes resource about the jwt

--- resources/reviews.py
+++ resources/reviews.py
@@ -6,23 +6,23 @@ from flask_jwt_extended import jwt_required
 
 
 class Reviews(Resource):
+    @jwt_required()
     def post(self):
         data = request.get_json()
         content = data.get("content")
         rating = data.get("rating")
         user_id = data.get("user_id")
         movie_id = data.get("movie_id")
-
+        current_user = get_jwt_identity()
         if not all([content, rating, user_id, movie_id]):
             return (
                 jsonify({"message": "All fields are required for review creation"}),
                 400,
             )
 
-        new_review = Review(content, rating, user_id, movie_id)
+        new_review = Review(content, rating, current_user, movie_id)
         db.session.add(new_review)
         db.session.commit()
-
         return jsonify({"message": "Review created successfully"}), 201
 
     class ReviewDetails(Resource):
