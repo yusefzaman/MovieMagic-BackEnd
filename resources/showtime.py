1784212e8d96b8fa69833b8c94142b1ae49299ec
Ghazialsardeeh <ghazialbakar@gmail.com>
2024-07-06 01:17:08 -0700

    model

---
+++ resources/showtime.py
@@ -0,0 +1,15 @@
+from app import db
+
+class Showtime(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    theatre_id = db.Column(db.String, db.ForeignKey('theatre.id'), nullable=False)
+    seats = db.Column(db.PickleType, nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    def __init__(self, id, theatre_id, seats, time, price):
+        self.id = id
+        self.theatre_id = theatre_id
+        self.seats = seats
+        self.time = time
+        self.price = price
