b643f7945baa9a5c297e8f4fea7540d24a2c3c76
Ghazialsardeeh <ghazialbakar@gmail.com>
2024-07-06 00:52:50 -0700

    ticket

--- resources/ticket.py
+++ resources/ticket.py
@@ -10,4 +10,11 @@ def add_ticket():
     user_id = data.get('user_id')
     movie_id = data.get('movie_id')
     showtime_id = data.get('showtime_id')
+    if not (id and user_id and movie_id and showtime_id):
+        return jsonify({'success': False, 'message': 'All fields are required'}), 400
+
+    ticket = Ticket(id=id, user_id=user_id, movie_id=movie_id, showtime_id=showtime_id)
+
+    db.session.add(ticket)
+    db.session.commit()
     
