98eda9f32e1a0434ceab096a8914f73da7c4e031
Ghazialsardeeh <ghazialbakar@gmail.com>
2024-07-06 01:41:27 -0700

    model

--- models/showtime.py
+++ models/showtime.py
@@ -3,3 +3,12 @@ from models.showtime import Showtime, db
 
 
 showtime_bp = Blueprint('showtime_bp', __name__)
+
+@showtime_bp.route('/add_showtime', methods=['POST'])
+def add_showtime():
+    data = request.json
+    id = data.get('id')
+    theatre_id = data.get('theatre_id')
+    seats = data.get('seats')
+    time = data.get('time')
+    price = data.get('price')
