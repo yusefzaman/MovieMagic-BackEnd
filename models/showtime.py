d5d8b5dc7ace73098f27864fac7c2f4602779834
Ghazialsardeeh <ghazialbakar@gmail.com>
2024-07-06 01:46:24 -0700

    model

--- models/showtime.py
+++ models/showtime.py
@@ -12,3 +12,12 @@ def add_showtime():
     seats = data.get('seats')
     time = data.get('time')
     price = data.get('price')
+
+    if not (id and theatre_id and seats and time and price):
+        return jsonify({'success': False, 'message': 'All fields are required'}), 400
+    showtime = Showtime(id=id, theatre_id=theatre_id, seats=seats, time=time, price=price)
+
+    db.session.add(showtime)
+    db.session.commit()
+
+    return jsonify({'success': True, 'message': 'Showtime added successfully'})
