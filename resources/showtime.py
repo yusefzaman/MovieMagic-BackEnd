52e1f9a0ff24792ebb4b97a841dba5438b0858ab
Ghazialsardeeh <ghazialbakar@gmail.com>
2024-07-06 02:12:38 -0700

    errors

--- resources/showtime.py
+++ resources/showtime.py
@@ -1,24 +1,37 @@
-from app import db
+from flask import Blueprint, request, jsonify
+from models.showtime import Showtime, db
 
-class Showtime(db.Model):
-    id = db.Column(db.String, primary_key=True)
-    theatre_id = db.Column(db.String, db.ForeignKey('theatre.id'), nullable=False)
-    seats = db.Column(db.PickleType, nullable=False)
-    time = db.Column(db.DateTime, nullable=False)
-    price = db.Column(db.Float, nullable=False)
 
-    def __init__(self, id, theatre_id, seats, time, price):
-        self.id = id
-        self.theatre_id = theatre_id
-        self.seats = seats if seats is not None else [False] * 60
-        self.time = time
-        self.price = price
+showtime_bp = Blueprint('showtime_bp', __name__)
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'theatre_id': self.theatre_id,
-            'seats': self.seats,
-            'time': self.time,
-            'price': self.price
-        }    
+@showtime_bp.route('/add_showtime', methods=['POST'])
+def add_showtime():
+    data = request.json
+    id = data.get('id')
+    theatre_id = data.get('theatre_id')
+    seats = data.get('seats')
+    time = data.get('time')
+    price = data.get('price')
+
+    if not (id and theatre_id and seats and time and price):
+        return jsonify({'success': False, 'message': 'All fields are required'}), 400
+    showtime = Showtime(id=id, theatre_id=theatre_id, seats=seats, time=time, price=price)
+
+    db.session.add(showtime)
+    db.session.commit()
+
+    return jsonify({'success': True, 'message': 'Showtime added successfully'})
+
+@showtime_bp.route('/get_showtimes', methods=['GET'])
+def get_showtimes():
+    showtimes = Showtime.query.all()
+    showtimes_data = [showtime.to_dict() for showtime in showtimes]
+    return jsonify(showtimes_data)
+
+@showtime_bp.route('/get_showtime/<showtime_id>', methods=['GET'])
+def get_showtime(showtime_id):
+    showtime = Showtime.query.get(showtime_id)
+    if not showtime:
+        return jsonify({'success': False, 'message': 'Showtime not found'}), 404
+
+    return jsonify(showtime.to_dict())
